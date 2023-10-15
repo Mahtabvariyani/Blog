f405bb2eb2a76174686d287205cf4d7ad620b515
Mah <mahtabvariyani@gmail.com>
2023-10-15 12:46:25 +0200

    fix css

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import styles from "./featured.module.css";
+import styles from "./styles.module.css";
 
 const Featured = () => {
   return (
