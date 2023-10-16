f5e09fa095bbfc74e37de5d518c89b823db792c3
Mah <mahtabvariyani@gmail.com>
2023-10-16 21:50:06 +0200

    fixed the GetAuth

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,8 +1,8 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import prisma from "./connect";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-
+import { getServerSession } from "next-auth";
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
@@ -10,12 +10,12 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-      
     }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-
   ],
 };
+
+export const getAuthSession = () => getServerSession(authOptions);
